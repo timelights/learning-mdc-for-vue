ffc32ed8bb99697e90dcbe114ce72407a9f4e688
Timelights <bhest.pat@gmail.com>
2019-04-05 15:59:15 +0800

    fixed first ui

--- src/App.vue
+++ src/App.vue
@@ -5,41 +5,10 @@
 </template>
 
 <script>
-import MDC101 from "@/components/MDC101.vue";
-import { MDCRipple } from "@material/ripple";
-import { MDCTextField } from "@material/textfield";
-import { MDCTopAppBar } from "@material/top-app-bar/index";
-
+import MDC101 from '@/components/MDC101.vue'
 export default {
   components: {
     MDC101
   },
-  mounted() {
-    // intanstiate the buttons with ripple
-    const buttons = document.querySelectorAll(".add-ripple");
-    for (let i = 0; i < buttons.length; i++) {
-      new MDCRipple(buttons[i]);
-    }
-    // instantiate textfields
-    const textfields = document.querySelectorAll(".mdc-text-field");
-    for (let i = 0; i < textfields.length; i++) {
-      new MDCTextField(textfields[i]);
-    }
-    const topAppBarElement = document.querySelector(".mdc-top-app-bar");
-    const topAppBar = new MDCTopAppBar(topAppBarElement);
-  }
 };
 </script>
-
-<style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700");
-@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
-@import "@material/button/mdc-button";
-
-html,
-body {
-  font-family: "Roboto", "Helvetica", "Arial";
-  padding: 0;
-  margin: 0;
-}
-</style>
--- src/components/MDC101.vue
+++ src/components/MDC101.vue
@@ -1,110 +1,108 @@
 <template>
   <div class="mdc101">
-    <header class="mdc-top-app-bar mdc-top-app-bar--fixed">
+    <header class="mdc-top-app-bar app-bar" id="app-bar">
       <div class="mdc-top-app-bar__row">
-        <section
-          class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start"
-        >
-          <a class="material-icons mdc-top-app-bar__navigation-icon">menu</a>
-          <span class="mdc-top-app-bar__title">Title</span>
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+          <a href="#" class="demo-menu material-icons mdc-top-app-bar__navigation-icon">menu</a>
+          <span class="mdc-top-app-bar__title">Dismissible Drawer</span>
         </section>
       </div>
     </header>
-
-    <aside class="mdc-drawer mdc-top-app-bar--fixed-adjust">
+    <aside class="mdc-drawer mdc-drawer--dismissible mdc-top-app-bar--fixed-adjust">
       <div class="mdc-drawer__content">
-        <nav class="mdc-list">
-          <a
-            class="mdc-list-item mdc-list-item--activated"
-            href="#"
-            aria-current="page"
-          >
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >inbox</i
-            >
+        <div class="mdc-list">
+          <a class="mdc-list-item mdc-list-item--activated" href="#" aria-current="page">
+            <i class="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>
             <span class="mdc-list-item__text">Inbox</span>
           </a>
           <a class="mdc-list-item" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >send</i
-            >
+            <i class="material-icons mdc-list-item__graphic" aria-hidden="true">send</i>
             <span class="mdc-list-item__text">Outgoing</span>
           </a>
           <a class="mdc-list-item" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true"
-              >drafts</i
-            >
+            <i class="material-icons mdc-list-item__graphic" aria-hidden="true">drafts</i>
             <span class="mdc-list-item__text">Drafts</span>
           </a>
-        </nav>
+        </div>
       </div>
     </aside>
 
-    <main>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        inventore libero unde eveniet! Perspiciatis sit sint modi omnis ea
-        libero vero, officia ut harum dolore beatae voluptatum. Dolor, sint
-        laborum?
-      </h1>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        inventore libero unde eveniet! Perspiciatis sit sint modi omnis ea
-        libero vero, officia ut harum dolore beatae voluptatum. Dolor, sint
-        laborum?
-      </h1>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        inventore libero unde eveniet! Perspiciatis sit sint modi omnis ea
-        libero vero, officia ut harum dolore beatae voluptatum. Dolor, sint
-        laborum?
-      </h1>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        inventore libero unde eveniet! Perspiciatis sit sint modi omnis ea
-        libero vero, officia ut harum dolore beatae voluptatum. Dolor, sint
-        laborum?
-      </h1>
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        inventore libero unde eveniet! Perspiciatis sit sint modi omnis ea
-        libero vero, officia ut harum dolore beatae voluptatum. Dolor, sint
-        laborum?
-      </h1>
-    </main>
-
-    <!-- <div class="mdc-layout-grid">
-        <div class="mdc-layout-grid__inner">
-            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-                <router-link to="/test">ABC</router-link>
-                <router-view/>
-            </div>
-            <div class="mdc-layout-grid__cell">DEF</div>
-            <div class="mdc-layout-grid__cell">GHI</div>
-        </div>
-        </div> -->
+    <div class="mdc-drawer-app-content mdc-top-app-bar--fixed-adjust">
+      <main class="main-content" id="main-content">
+        App Content
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import { MDCList } from "@material/list";
-import { MDCDrawer } from "@material/drawer";
+import {MDCDrawer} from "@material/drawer";
+import {MDCTopAppBar} from "@material/top-app-bar";
 
 export default {
   data() {
     return {};
   },
   mounted() {
-    const list = MDCList.attachTo(document.querySelector(".mdc-list"));
-    list.wrapFocus = true;
-    const drawer = MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
+    // instantiate drawer
+    const drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+    // toggle drawer when clicking on the nav button in the top-app-bar
+    const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+    topAppBar.setScrollTarget(document.getElementById('main-content'));
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+      drawer.open = !drawer.open;
+    });
+    // Close the drawer when an item is activated in order to dismiss the modal
+    // as soon as the user performs an action. Only restore focus to the first
+    // focusable element in the main content after the drawer is closed, since
+    // it's being closed automatically.
+    const listEl = document.querySelector('.mdc-drawer .mdc-list');
+    const mainContentEl = document.querySelector('.main-content');
+
+    listEl.addEventListener('click', (event) => {
+      drawer.open = false;
+    });
+
+    document.body.addEventListener('MDCDrawer:closed', () => {
+      mainContentEl.querySelector('input, button').focus();
+    });
   }
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 @import "@material/top-app-bar/mdc-top-app-bar";
-@import "@material/layout-grid/mdc-layout-grid";
 @import "@material/drawer/mdc-drawer";
 @import "@material/list/mdc-list";
+
+html, body {
+  margin: 0;
+}
+
+body,
+.mdc101 {
+  display: flex;
+  height: 100vh;
+}
+
+.mdc-drawer-app-content {
+  flex: auto;
+  overflow: auto;
+  position: relative;
+}
+
+.main-content {
+  overflow: auto;
+  height: 100%;
+}
+
+.app-bar {
+  position: absolute;
+}
+
+// only apply this style if below top app bar
+.mdc-top-app-bar {
+  z-index: 7;
+}
 </style>
